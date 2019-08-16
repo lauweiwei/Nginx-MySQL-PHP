5f47adc48702905de446d50be3b568bd0f9de482
lauweiwei <viluu1997@gmail.com>
2019-08-16 13:56:31 +0700

    bash shell

--- nginx-mysql-php.sh
+++ nginx-mysql-php.sh
@@ -1,3 +1,8 @@
+# clone git
+  mkdir /root/downloads
+  cd downloads
+  git clone https://github.com/lauweiwei/Nginx-MySQL-PHP.git
+  cd
 #check package installed
 systemctl status mysqld > /dev/null
 if [ $? -eq 0 ]; then
@@ -43,81 +48,76 @@ if [ $? -eq 0 ]; then
   echo "php-fpm service is installed"
 else
   echo "php-fpm service is not installed"
-  yum install php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache 
+  yum -y install php72-php-fpm php72-php-gd php72-php-json php72-php-mbstring php72-php-mysqlnd php72-php-xml php72-php-xmlrpc php72-php-opcache 
 fi
 
 #check github config
-  mkdir /root/downloads
-  cd downloads
-  git clone https://github.com/lauweiwei/Nginx-MySQL-PHPfpm-CentOS7.git
-  cd 
-
-  nginx1=$(md5sum /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/default1.conf | cut -c1-33)
+  nginx1=$(md5sum /root/downloads/Nginx-MySQL-PHP/default1.conf | cut -c1-33)
   nginx2=$(md5sum /etc/nginx/conf.d/default1.conf | cut -c1-33)
   if [ "$nginx1" == "$nginx2" ]; then
     echo "Files are equal"
   else
     echo "Files are not equal"
-    mv /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/default1.conf /etc/nginx/conf.d/default1.conf
+    mv /root/downloads/Nginx-MySQL-PHP/default1.conf /etc/nginx/conf.d/default1.conf
     echo "File updated!"
   fi
 
-  php1=$(md5sum /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/index.php | cut -c1-33)
+  php1=$(md5sum /root/downloads/Nginx-MySQL-PHP/index.php | cut -c1-33)
   php2=$(md5sum /var/www/php1/index.php | cut -c1-33)
   if [ "$php1" == "$php2" ]; then
     echo "Files are equal"
   else
     echo "Files are not equal"
-    mv /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/index.php /var/www/php1/index.php
+    mv /root/downloads/Nginx-MySQL-PHP/index.php /var/www/php1/index.php
     echo "File updated!"
   fi
 
-  mysql1=$(md5sum /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/my.cnf | cut -c1-33)
+  mysql1=$(md5sum /root/downloads/Nginx-MySQL-PHP/my.cnf | cut -c1-33)
   mysql2=$(md5sum /etc/my.cnf| cut -c1-33)
   if [ "$mysql1" == "$mysql2" ]; then
     echo "Files are equal"
   else
     echo "Files are not equal"
-    mv /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/my.cnf /etc/my.cnf
+    mv /root/downloads/Nginx-MySQL-PHP/my.cnf /etc/my.cnf
     echo "File updated!"
   fi
 
-  nginx3=$(md5sum /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/nginx.conf | cut -c1-33)
+  nginx3=$(md5sum /root/downloads/Nginx-MySQL-PHP/nginx.conf | cut -c1-33)
   nginx4=$(md5sum /etc/nginx/nginx.conf | cut -c1-33)
   if [ "$nginx3" == "$nginx4" ]; then
     echo "Files are equal"
   else
     echo "Files are not equal"
-    mv /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/nginx.conf /etc/nginx/nginx.conf
+    mv /root/downloads/Nginx-MySQL-PHP/nginx.conf /etc/nginx/nginx.conf
     echo "File updated!"
   fi
 
-  php3=$(md5sum /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/php.ini | cut -c1-33)
+  php3=$(md5sum /root/downloads/Nginx-MySQL-PHP/php.ini | cut -c1-33)
   php4=$(md5sum /etc/opt/remi/php72/php.ini | cut -c1-33)
   if [ "$php3" == "$php4" ]; then
     echo "Files are equal"
   else
     echo "Files are not equal"
-    mv /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/php.ini /etc/opt/remi/php72/php.ini
+    mv /root/downloads/Nginx-MySQL-PHP/php.ini /etc/opt/remi/php72/php.ini
     echo "File updated!"
   fi
 
-  php5=$(md5sum /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/www.conf | cut -c1-33)
+  php5=$(md5sum /root/downloads/Nginx-MySQL-PHP/www.conf | cut -c1-33)
   php6=$(md5sum /etc/opt/remi/php72/php-fpm.d/www.conf | cut -c1-33)
   if [ "$php5" == "$php6" ]; then
     echo "Files are equal"
   else
     echo "Files are not equal"
-    mv /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/www.conf /etc/opt/remi/php72/php-fpm.d/www.conf
+    mv /root/downloads/Nginx-MySQL-PHP/www.conf /etc/opt/remi/php72/php-fpm.d/www.conf
     echo "File updated!"
   fi
 
-  php7=$(md5sum /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/php-fpm.conf | cut -c1-33)
+  php7=$(md5sum /root/downloads/Nginx-MySQL-PHP/php-fpm.conf | cut -c1-33)
   php8=$(md5sum /etc/opt/remi/php72/php-fpm.conf | cut -c1-33)
   if [ "$php7" == "$php8" ]; then
     echo "Files are equal"
   else
     echo "Files are not equal"
-    mv /root/downloads/Nginx-MySQL-PHPfpm-CentOS7/php-fpm.conf /etc/opt/remi/php72/php-fpm.conf
+    mv /root/downloads/Nginx-MySQL-PHP/php-fpm.conf /etc/opt/remi/php72/php-fpm.conf
     echo "File updated!"
   fi
